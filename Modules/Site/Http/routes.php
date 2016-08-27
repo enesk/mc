eacb7c80159590b4309c9abd57b54794f5beae03
Enes <kul.enes@gmail.com>
2016-08-28 01:18:07 +0200

    sale complated

--- Modules/Site/Http/routes.php
+++ Modules/Site/Http/routes.php
@@ -6,6 +6,6 @@ Route::group(['prefix' => 'api', 'as' => 'api::', 'namespace' => 'Modules\Site\H
 });
 
 
-Route::group(['middleware' => 'web'], function () {
+Route::group(['middleware' => 'web', 'namespace' => 'Modules\Site\Http\Controllers'], function () {
     Route::get('{slug}', ['uses' => 'Modules\Site\Http\Controllers\PagesController@getPage'])->where('slug', '([A-Za-z0-9\-\/]+)');
 });
