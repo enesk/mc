02fc3d72ab5abdf9a11fdf62a03e647f8f301617
Enes <kul.enes@gmail.com>
2016-09-15 13:25:47 +0200

    mail send

--- modules/Car/Http/Controllers/EmailsController.php
+++ modules/Car/Http/Controllers/EmailsController.php
@@ -3,10 +3,18 @@
 namespace Modules\Car\Http\Controllers;
 
 use App\Http\Controllers\Controller;
+use Mail;
 
 class EmailsController extends Controller
 {
-   public function interested(\Request $request) {
-        return \Response::json($request::all());
-   }
+    public function interested(\Request $request)
+    {
+        $mail = Mail::send('emails.interested', ['data' => (object)$request::all()], function ($m) {
+            $m->from('kul@widimedia.com', 'Millennium Cars Website');
+
+            $m->to('kul@widimedia.com', 'Enes Kul')->subject('Ein Käufer interessiert sich für ein Inserat!');
+        });
+
+        return \Response::json($mail);
+    }
 }
