ec358ab7cb3fe4ed4d60f4c7e71f3feedc3ddb88
Enes <kul.enes@gmail.com>
2016-09-17 13:14:48 +0200

    seo text

--- public/themes/frontend/default/views/partials/footers/footer.blade.php
+++ public/themes/frontend/default/views/partials/footers/footer.blade.php
@@ -66,27 +66,18 @@
     <div class="container footer_bottom_desc">
         <div class="row mt20">
             <div class="col-lg-6 col-lg-offset-3 text-center">
-                <h1 class="titleNegative">SEO TITLE</h1>
+                <h1 class="titleNegative">Millennium Cars</h1>
             </div>
         </div>
         <div class="row">
             <div class="col-lg-12">
-                <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean
-                    massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec
-                    quam felis, ultricies nec, pellentesque eu,
-                    pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec,
-                    vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum
-                    felis eu pede mollis pretium. Integer tincidunt.
-                    Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula,
-                    porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis,
-                    feugiat a, tellus. Phasellus viverra nulla ut metus
-                    varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur
-                    ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum
-                    rhoncus, sem quam semper libero, sit amet adipiscing sem
-                    neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio
-                    et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam
-                    sit amet orci eget eros faucibus tincidunt. Duis
-                    leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna.</p>
+                <p>LUnternehmenserfahrung seit über 30 Jahren und über 50 000 Verkauften PKWs. Autoverkauf in drei
+
+                    Filialen. Über 500 PKWS auf Lager. Baden-Baden, Rastatt, Karlsruhe. Finanzierung ab 0,9% möglich.
+
+                    Im Angebot Neuwagen und Jahreswagen. Fahrzeuge aller Klassen. Autovermietung in 3 Standorten.
+
+                    Kleinwagen, Mittelklassewagen sowie Luxusfahrzeuge.</p>
             </div>
         </div>
 
